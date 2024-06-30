41205a77d48315639a633e5626fa863066149c93
Anshika Pathak <anshikapathakcs@gmail.com>
2024-06-30 14:56:51 +0530

    Added Footer cell

---
+++ Compositional Layout/Composition Layout/HeaderFooterCell/Header.swift
@@ -0,0 +1,17 @@
+//
+//  Header.swift
+//  Compositional Layout
+//
+//  Created by OS on 30/06/24.
+//
+
+import UIKit
+
+class Header: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Compositional Layout/Composition Layout/HeaderFooterCell/HeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderView.swift
+//  Compositional Layout
+//
+//  Created by OS on 30/06/24.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
