b1586a86fec817465454ccbec0c38572c56f2bfa
Anshika Pathak <anshikapathakcs@gmail.com>
2024-06-30 14:38:31 +0530

    Added Banner Cell file and xib

---
+++ Compositional Layout/Composition Layout/BannerCell/BannerCvCell.swift
@@ -0,0 +1,18 @@
+//
+//  BannerCvCell.swift
+//  Compositional Layout
+//
+//  Created by OS on 30/06/24.
+//
+
+import UIKit
+
+class BannerCvCell: UICollectionViewCell {
+    @IBOutlet var parentView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.parentView.layer.cornerRadius = 12
+        // Initialization code
+    }
+
+}
